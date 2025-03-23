5a47c6f240dff4ef973cc2c17c95320bf7af57dc
BPChance <bradenpchance6@gmail.com>
2025-03-23 11:48:00 -0500

    made env variables available in server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const routes = require('./routes/index');
+const { configDotenv } = require('dotenv');
+
+configDotenv();
 
 const app = express();
 const port = process.env.PORT || 5000;
