2c6fbfa435aad049296704f41a2f4e78c9745aea
BPChance <bradenpchance6@gmail.com>
2025-03-24 17:28:35 -0500

    added higher limit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ configDotenv();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use('/', routes);
 
 app.listen(port, () => {
